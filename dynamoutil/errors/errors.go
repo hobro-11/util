b0d789e1ea72fa564c60a506a7c00eedcabfff72
Hobro <hobro3710@naver.com>
2025-07-02 13:54:10 +0900

    create sentinel err

--- dynamoutil/errors/errors.go
+++ dynamoutil/errors/errors.go
@@ -2,22 +2,30 @@ package errors
 
 import (
 	"errors"
+	"fmt"
 
-	"github.com/aws/aws-sdk-go-v2/aws/transport/http"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/aws/smithy-go"
+	"github.com/aws/smithy-go/transport/http"
 )
 
 type ApiError struct {
-	Code    int
 	Message string
-	Reason  []string
-	Err     error
 }
 
 const (
-	ValidationException = "ValidationException"
+	ValidationException             = "ValidationException"
+	ValidationError                 = "ValidationError"
 	ConditionalCheckFailedException = "ConditionalCheckFailedException"
+	TransactionCanceledException    = "TransactionCanceledException"
+	ConditionalReqFailedMessage     = "The conditional request failed"
+)
+
+var (
+	ErrConditionFailed  = errors.New("condition failed")
+	ErrValidationFailed = errors.New("validation failed")
+	ErrInternalError    = errors.New("internal error")
+	ErrUnknown          = errors.New("unknown error")
 )
 
 // Error는 에러 인터페이스를 구현합니다
@@ -25,87 +33,56 @@ func (e *ApiError) Error() string {
 	return e.Message
 }
 
-func (e *ApiError) Is(target error) bool {
-	_, ok := target.(*ApiError)
-	return ok
-}
-
 func ErrorHandle(inputErr error) error {
-	var code int
-	var httpErr *http.ResponseError
+	var (
+		code     int
+		httpErr  *http.ResponseError
+		apiError smithy.APIError
+	)
+
 	if errors.As(inputErr, &httpErr) {
 		code = httpErr.Response.StatusCode
-	} else {
-		return inputErr
-	}
-
-	var txApiErr *types.TransactionCanceledException
-	if errors.As(inputErr, &txApiErr) {
-		reasons := make([]string, 0)
-		for _, reason := range txApiErr.CancellationReasons {
-			if reason.Message == nil {
-				continue
-			}
-			if *reason.Code == "ValidationError" {
-				return &ApiError{
-					Code:    code,
-					Message: ValidationException,
-					Reason:  []string{*reason.Message},
-					Err:     txApiErr,
-				}
-			}
-			if *reason.Code == "ConditionalCheckFailed" {
-				return &ApiError{
-					Code:    code,
-					Message: ConditionalCheckFailedException,
-					Reason:  []string{*reason.Message},
-					Err:     txApiErr,
-				}
-			}
-			reasons = append(reasons, *reason.Message)
-		}
-		return &ApiError{
-			Code:    code,
-			Message: "TransactionCanceled",
-			Reason:  reasons,
-			Err:     txApiErr,
+		if code == 500 {
+			code = 500
 		}
 	}
 
-	var condCheckFailed *types.ConditionalCheckFailedException
-	if errors.As(inputErr, &condCheckFailed) {
-		return &ApiError{
-			Code:    code,
-			Message: ConditionalCheckFailedException,
-			Reason:  []string{*condCheckFailed.Message},
-			Err:     condCheckFailed,
-		}
-	}
-
-	var apiError smithy.APIError
 	if errors.As(inputErr, &apiError) {
-		if apiError.ErrorCode() == "ValidationException" {
-			return &ApiError{
-				Code:    code,
-				Message: ValidationException,
-				Reason:  []string{apiError.ErrorMessage()},
-				Err:     apiError,
+		if code == 500 {
+			return fmt.Errorf("%w: %w", ErrInternalError, apiError)
+		}
+
+		if apiError.ErrorCode() == ValidationException {
+			return fmt.Errorf("%w: %w", ErrValidationFailed, apiError)
+		}
+
+		if apiError.ErrorCode() == ConditionalCheckFailedException {
+			return fmt.Errorf("%w: %w", ErrConditionFailed, apiError)
+		}
+
+		var txApiErr *types.TransactionCanceledException
+		if errors.As(inputErr, &txApiErr) {
+			reason := txApiErr.CancellationReasons[0]
+			if reason.Message != nil {
+				if *reason.Message == ConditionalReqFailedMessage {
+					return fmt.Errorf("%w: %w", ErrConditionFailed, txApiErr)
+				}
 			}
-		}
-		if apiError.ErrorCode() == "ConditionalCheckFailedException" {
-			return &ApiError{
-				Code:    code,
-				Message: ConditionalCheckFailedException,
-				Reason:  []string{apiError.ErrorMessage()},
-				Err:     apiError,
+
+			if reason.Code != nil {
+				if *reason.Code == ValidationError {
+					return fmt.Errorf("%w: %w", ErrValidationFailed, txApiErr)
+				}
 			}
+
+			return fmt.Errorf("%w: %w", ErrUnknown, txApiErr)
 		}
-		return &ApiError{
-			Code:    code,
-			Message: apiError.ErrorCode(),
-			Reason:  []string{apiError.ErrorMessage()},
-			Err:     apiError,
-		}
+
+		return fmt.Errorf("%w: %w", ErrUnknown, apiError)
 	}
-	return inputErr
+
+	if code == 500 {
+		return fmt.Errorf("%w: %w", ErrInternalError, inputErr)
+	}
+	return fmt.Errorf("%w: %w", ErrUnknown, inputErr)
 }
